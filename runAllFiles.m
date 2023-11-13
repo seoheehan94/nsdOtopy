882ad2f3fba7f52af4460e713cb0a5d1a197fd21
osooni2 <39323129+osooni2@users.noreply.github.com>
2023-11-13 17:31:17 -0500

    up

---
+++ runAllFiles.m
@@ -0,0 +1,19 @@
+%% prfSampleModel
+for sub = 2:8
+    fprintf('%s. %d. %d ...\n','prfSampleModel',sub,1);
+    prfSampleModel(sub,1);
+    fprintf('%s. %d. %d ...\n','prfSampleModel',sub,2);
+    prfSampleModel(sub,2);
+    fprintf('%s. %d. %d ...\n','prfSampleModel',sub,3);
+    prfSampleModel(sub,3);
+    fprintf('%s. %d. %d ...\n','prfSampleModel',sub,4);
+    prfSampleModel(sub,4);
+end
+
+%% regressPrfSplit
+for sub = 2:8
+
+fprintf('%s. %d. %d ...\n','regressPrfSplit',sub,1);
+regressPrfSplit(sub, 4);
+
+end
