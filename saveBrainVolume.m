6c23585fa857f04f1dc59d088c39f16b0093e124
osooni2 <39323129+osooni2@users.noreply.github.com>
2024-01-12 15:44:58 -0500

    update

--- saveBrainVolume.m
+++ saveBrainVolume.m
@@ -1,6 +1,12 @@
-cd '/home/hanseohe/Documents/GitHub/nsdOtopy'
+%%cd '/home/hanseohe/Documents/GitHub/nsdOtopy'
+cd '/bwlab/Users/SeoheeHan/NSDData/rothzn/nsd/brainVolume';
+corrList = [];
+for sub = 1:8
 
-load('/bwlab/Users/SeoheeHan/NSDData/rothzn/nsd/prfsample_Ori/voxModelPref_sub1.mat');
+
+%%
+fileName = ['/bwlab/Users/SeoheeHan/NSDData/rothzn/nsd/prfsample_Ori/voxModelPref_sub', num2str(sub), '.mat'];
+load(fileName);
 
 ourBrain = visRoiData;
 ourBrain(ourBrain == 2) = 1;
@@ -19,28 +25,44 @@ angleBrain(angleBrain < 0) = -1;
 
 %hist(angleBrain);
 
-
-save('angleBrain_new.mat', 'angleBrain');
+saveName = ['angleBrain_sub', num2str(sub), 'mat'];
+save(saveName, 'angleBrain');
 
 %%
-clear all;
-cd '/bwlab/Users/SeoheeHan/NSDData/rothzn/nsd/brainVolume'
-load("angleBrain_new.mat");
+%clear all;
+%cd '/bwlab/Users/SeoheeHan/NSDData/rothzn/nsd/brainVolume'
+%load("angleBrain_new.mat");
 size(angleBrain)
 
 %3dinfo betas_session01.nii.gz 
 %3dcalc -a betas_session01.nii.gz[1] -expr a -prefix oneBeta
+command = ['3dinfo betas_session01_sub', num2str(sub), '.nii.gz'];
+system(command);
+command = ['3dcalc -a betas_session01_sub', num2str(sub), '.nii.gz[1] -expr a -prefix oneBeta_sub', num2str(sub)];
+system(command);
 
-[err,V,Info] = BrikLoad('oneBeta+orig');
-Info.RootName = 'angleBrain_new+orig';
-opt.Prefix = 'angleBrain_new';
+currOneBeta = ['oneBeta_sub', num2str(sub), '+orig'];
+[err,V,Info] = BrikLoad(currOneBeta);
+Info.RootName = ['angleBrain_sub', num2str(sub), '+orig'];
+opt.Prefix = ['angleBrain_sub', num2str(sub)];
 WriteBrik(angleBrain,Info,opt);
 
-%%
-load('/bwlab/Users/SeoheeHan/NSDData/rothzn/nsd/prfsample_Ori/voxModelPref_sub1.mat');
+% compare original and new
 roiOri_New = roiOri;
-load('/bwlab/Users/SeoheeHan/NSDData/rothzn/nsd/prfsample/voxModelPref_sub1.mat');
+origfileName = ['/bwlab/Users/SeoheeHan/NSDData/rothzn/nsd/prfsample/voxModelPref_sub', num2str(sub), '.mat'];
+load(origfileName);
 
 original = roiOri{1,1}(3,:)';
 new = roiOri_New{1,1}(3,:)';
 [r,p] = corr(original, new);
+
+corrList = [corrList; r, p];
+end
+%%
+% load('/bwlab/Users/SeoheeHan/NSDData/rothzn/nsd/prfsample_Ori/voxModelPref_sub1.mat');
+% roiOri_New = roiOri;
+% load('/bwlab/Users/SeoheeHan/NSDData/rothzn/nsd/prfsample/voxModelPref_sub1.mat');
+% 
+% original = roiOri{1,1}(3,:)';
+% new = roiOri_New{1,1}(3,:)';
+% [r,p] = corr(original, new);
