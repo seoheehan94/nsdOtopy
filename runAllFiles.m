6e4281a9c1f9b79623f398ab914b5d650f297854
osooni2 <39323129+osooni2@users.noreply.github.com>
2024-01-26 14:51:07 -0500

    update

--- runAllFiles.m
+++ runAllFiles.m
@@ -1,36 +1,31 @@
 %% prfSampleModel
 
 addpath(genpath('/home/hanseohe/Documents/GitHub/stimulusVignetting'))
-for sub = 3:8
-    fprintf('%s. %d. %d ...\n','prfSampleModel',sub,1);
-    prfSampleModel(sub,1);
-    fprintf('%s. %d. %d ...\n','prfSampleModel',sub,2);
-    prfSampleModel(sub,2);
-    fprintf('%s. %d. %d ...\n','prfSampleModel',sub,3);
-    prfSampleModel(sub,3);
-    fprintf('%s. %d. %d ...\n','prfSampleModel',sub,4);
-    prfSampleModel(sub,4);
-end
-
-% %% regressPrfSplit
-% for sub = 5:8
-% fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 1);
-% regressPrfSplit(sub, 1);
-% fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 2);
-% regressPrfSplit(sub, 2);
-% fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 3);
-% regressPrfSplit(sub, 3);
-% fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 4);
-% regressPrfSplit(sub, 4);
-% 
+% for sub = 3:8
+%     fprintf('%s. %d. %d ...\n','prfSampleModel',sub,1);
+%     prfSampleModel(sub,1);
+%     fprintf('%s. %d. %d ...\n','prfSampleModel',sub,2);
+%     prfSampleModel(sub,2);
+%     fprintf('%s. %d. %d ...\n','prfSampleModel',sub,3);
+%     prfSampleModel(sub,3);
+%     fprintf('%s. %d. %d ...\n','prfSampleModel',sub,4);
+%     prfSampleModel(sub,4);
 % end
-% 
-% %% 
-% getVoxPref(1,4)
-% getVoxPref(2,4)
-% getVoxPref(3,4)
-% getVoxPref(4,4)
-% getVoxPref(5,4); 
-% getVoxPref(6,4); 
-% getVoxPref(7,4); 
-% getVoxPref(8,4);
+
+%% regressPrfSplit
+for sub = 2:2
+fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 1);
+regressPrfSplit(sub, 1);
+fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 2);
+regressPrfSplit(sub, 2);
+fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 3);
+regressPrfSplit(sub, 3);
+fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 4);
+regressPrfSplit(sub, 4);
+
+end
+%
+%% getVoxPref
+for sub = 2:2
+    getVoxPref(sub,4)
+end
