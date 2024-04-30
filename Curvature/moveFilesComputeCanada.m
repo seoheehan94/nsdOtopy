270e4a911b3e5c2af717c8200269b7f2fa409096
seoheehan94 <seohee.han@mail.utoronto.ca>
2024-04-30 13:04:26 -0400

    Update moveFilesComputeCanada.m

--- Curvature/moveFilesComputeCanada.m
+++ Curvature/moveFilesComputeCanada.m
@@ -1,8 +1,8 @@
-
+%moveFilesComputeCanada.m
 
 cd '/bwlab/Users/SeoheeHan/NSDData/rothzn/nsd/stimuli';
 
-command = 'scp -r curfilter/ hanseohe@beluga4.computecanada.ca:/home/hanseohe/scratch/stimuli/curvfilter';
+command = 'scp -r curvfilter/ hanseohe@beluga4.computecanada.ca:/home/hanseohe/scratch/stimuli';
 system(command);
 
 
