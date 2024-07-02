ae358e3996bd0279657bbe8adc1f3b644b9d2d83
seoheehan94 <seohee.han@mail.utoronto.ca>
2024-07-02 09:58:57 -0400

    Create runAllFiles_curvatureMLV.m

---
+++ curvature_MLV/runAllFiles_curvatureMLV.m
@@ -0,0 +1,51 @@
+%% prfSampleModel_curvature
+
+addpath(genpath('/home/hanseohe/Documents/GitHub/stimulusVignetting'))
+addpath(genpath('/home/hanseohe/Documents/GitHub/nsdOtopy'))
+% 
+% for sub = 5:8
+%     tic;
+%     fprintf('%s. %d. %d ...\n','prfSampleModel',sub,1);
+%     prfSampleModel_length(sub,1);
+%     toc;
+% 
+%     tic;
+%     fprintf('%s. %d. %d ...\n','prfSampleModel',sub,2);
+%     prfSampleModel_length(sub,2);
+%     toc;
+% 
+%     tic;
+%     fprintf('%s. %d. %d ...\n','prfSampleModel',sub,3);
+%     prfSampleModel_length(sub,3);
+%     toc;
+% 
+%     tic;
+%     fprintf('%s. %d. %d ...\n','prfSampleModel',sub,4);
+%     prfSampleModel_length(sub,4);
+%     toc;
+% end
+
+%% regressPrfSplit
+for sub = 2:8
+fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 1);
+regressPrfSplit_curvatureMLV(sub, 1);
+fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 2);
+regressPrfSplit_curvatureMLV(sub, 2);
+fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 3);
+regressPrfSplit_curvatureMLV(sub, 3);
+fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 4);
+regressPrfSplit_curvatureMLV(sub, 4);
+
+fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 5);
+regressPrfSplit_curvatureMLV(sub, 5);
+fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 6);
+regressPrfSplit_curvatureMLV(sub, 6);
+fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 7);
+regressPrfSplit_curvatureMLV(sub, 7);
+
+end
+
+%% getVoxPref
+% for sub = 1:8
+%     getVoxPref_curvature(sub,7)
+% end
