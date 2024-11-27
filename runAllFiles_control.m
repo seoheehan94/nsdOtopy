592686c4c27ac0e6776bdc9e8ea60c32558a5784
seoheehan94 <seohee.han@mail.utoronto.ca>
2024-11-27 09:33:46 -0500

    update

--- runAllFiles_control.m
+++ runAllFiles_control.m
@@ -11,29 +11,29 @@
 % end
 
 %% regressPrfSplit
-for sub = 1:8
-fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 1);
-regressPrfSplit(sub, 1);
-fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 2);
-regressPrfSplit(sub, 2);
-fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 3);
-regressPrfSplit(sub, 3);
-fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 4);
-regressPrfSplit(sub, 4);
-end
-
-for sub = 1:8
-fprintf('%s. %d. %d ...\n','regressPrfSplit_control',sub, 1);
-regressPrfSplit_control(sub, 1);
-fprintf('%s. %d. %d ...\n','regressPrfSplit_control',sub, 2);
-regressPrfSplit_control(sub, 2);
-fprintf('%s. %d. %d ...\n','regressPrfSplit_control',sub, 3);
-regressPrfSplit_control(sub, 3);
-fprintf('%s. %d. %d ...\n','regressPrfSplit_control',sub, 4);
-regressPrfSplit_control(sub, 4);
-end
+% for sub = 1:8
+% fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 1);
+% regressPrfSplit(sub, 1);
+% fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 2);
+% regressPrfSplit(sub, 2);
+% fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 3);
+% regressPrfSplit(sub, 3);
+% fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 4);
+% regressPrfSplit(sub, 4);
+% end
+% 
+% for sub = 1:8
+% fprintf('%s. %d. %d ...\n','regressPrfSplit_control',sub, 1);
+% regressPrfSplit_control(sub, 1);
+% fprintf('%s. %d. %d ...\n','regressPrfSplit_control',sub, 2);
+% regressPrfSplit_control(sub, 2);
+% fprintf('%s. %d. %d ...\n','regressPrfSplit_control',sub, 3);
+% regressPrfSplit_control(sub, 3);
+% fprintf('%s. %d. %d ...\n','regressPrfSplit_control',sub, 4);
+% regressPrfSplit_control(sub, 4);
+% end
 
 %% getVoxPref_control
-% for sub = 2:8
-%     getVoxPref_control(sub,4);
-% end
+for sub = 1:8
+    getVoxPref_regress(sub,4);
+end
