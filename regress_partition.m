01733c1d3608a50787dc52eb806012756745ef2d
seoheehan94 <seohee.han@mail.utoronto.ca>
2024-12-10 18:52:02 -0500

    Update regress_partition.m

--- regress_partition.m
+++ regress_partition.m
@@ -146,7 +146,7 @@ for isub=1:8
                     voxPrfOriSample2(:,end+1) = ones;
 
                     pred_model1{roinum}(isplit,ivox,1:numTrials) = squeeze(voxOriCoef1{roinum}(nsplits-isplit+1,ivox,:))'*voxPrfOriSample1';
-                    residuals_model1{roinum}(isplit,ivox,1:numTrials) = voxBetas' - pred_model1{roinum}(isplit,ivox,1:numTrials);
+                    residuals_model1{roinum}(isplit,ivox,1:numTrials) = voxBetas' - squeeze(voxOriCoef1{roinum}(nsplits-isplit+1,ivox,:))'*voxPrfOriSample1';
 
                     pred_model2_orth{roinum}(isplit,ivox,1:numTrials) = squeeze(coef_model2_orth{roinum}(nsplits-isplit+1,ivox,:))'*voxPrfOriSample2';
                     residuals_model2_orth{roinum}(isplit,ivox,1:numTrials) =  residuals_model1{roinum}(isplit,ivox,1:numTrials) - pred_model2_orth{roinum}(isplit,ivox,1:numTrials);
